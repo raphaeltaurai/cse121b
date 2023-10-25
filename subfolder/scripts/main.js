75d4511cf034cda3a65da0a5768c6233aad62e6a
raphaeltaurai <tau22023@byui.edu>
2023-10-25 08:04:55 +0200

    final project

--- subfolder/scripts/main.js
+++ subfolder/scripts/main.js
@@ -1,4 +1,4 @@
 document.getElementById("calculate").addEventListener("click", function() {
     var mathExpression = document.getElementById("math").value;
-    giveResult(mathExpression);
+    giveResult(mathExpression, "http://api.mathjs.org/v4/?expr=", "There's an issue with the API");
 });
--- subfolder/scripts/projects.js
+++ subfolder/scripts/projects.js
@@ -1,18 +1,16 @@
-export function calculateResult(mathExpression) {
-    const apiUrl = "http://api.mathjs.org/v4/?expr=";
-  
-    fetch(apiUrl + encodeURIComponent(mathExpression))
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("API request failed");
-        }
-        return response.json();
+function giveResult(mathExpression, apiEndpoint, errorMessage) {
+  fetch(apiEndpoint + encodeURIComponent(mathExpression))
+      .then((response) => {
+          if (!response.ok) {
+              throw new Error("API request failed");
+          }
+          return response.json();
       })
-      .then(data => {
-        document.getElementById("result").innerHTML = "Result: " + data.result;
+      .then((data) => {
+          document.getElementById("result").innerHTML = data.result;
       })
-      .catch(error => {
-        document.getElementById("result").innerHTML = "Error: There's an issue with the API";
-        console.error(error);
+      .catch((error) => {
+          document.getElementById("result").innerHTML = "Error: " + errorMessage;
+          console.error(error);
       });
-  }
+}
