213ae4e49de3c7fc6c2715c016207d8d180517e2
raphaeltaurai <tau22023@byui.edu>
2023-10-25 08:00:42 +0200

    final

--- subfolder/scripts/projects.js
+++ subfolder/scripts/projects.js
@@ -1,16 +1,18 @@
-function giveResult(mathExpression) {
-    fetch("http://api.mathjs.org/v4/" + encodeURIComponent(mathExpression))
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("API request failed");
-            }
-            return response.json();
-        })
-        .then((data) => {
-            document.getElementById("result").innerHTML = data.result;
-        })
-        .catch((error) => {
-            document.getElementById("result").innerHTML = "Error: There's an issue with the API";
-            console.error(error);
-        });
-}
+export function calculateResult(mathExpression) {
+    const apiUrl = "http://api.mathjs.org/v4/?expr=";
+  
+    fetch(apiUrl + encodeURIComponent(mathExpression))
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("API request failed");
+        }
+        return response.json();
+      })
+      .then(data => {
+        document.getElementById("result").innerHTML = "Result: " + data.result;
+      })
+      .catch(error => {
+        document.getElementById("result").innerHTML = "Error: There's an issue with the API";
+        console.error(error);
+      });
+  }
